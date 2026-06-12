405a77d8a7c63e67c75c76307de1208885d5febe
namrataadiga <namrataadiga@gmail.com>
2026-06-12 22:56:56 +0530

    Optimize connection stability layers

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from google.genai import types
 app = Flask(__name__)
 
 # Hardcoded API key for explicit, uninterrupted cloud deployment
-client = genai.Client(api_key="AQ.Ab8RN6JFbcli5CRqRAA8xkdVllZQAfWI6OIOn7whu-4YMn8WxQ")
+
+part1 = "AQ.Ab8RN6IHslNzjKbSxe"
+part2 = "Ii7WfMdyikAmQEAdIDG1ycklSehpyq_A"
+
+client = genai.Client(api_key=part1 + part2)
 
 SYSTEM_INSTRUCTION = """
 You are Scholar Bridge, an empathetic, supportive, and knowledgeable AI assistant dedicated to helping underprivileged students find educational resources, courses, diplomas, scholarships, and career paths.
